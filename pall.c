087862a6b2ffb1e915d38b4154a414b7991b3672
akinmode <akinjil@gmail.com>
2024-03-28 20:35:54 +0000

    Added push&pall

---
+++ pall.c
@@ -0,0 +1,24 @@
+#include "monty.h"
+
+/**
+ * pall - Prints all the values on the stack,
+ * starting from the top of the stack.
+ * @stack: The head of the stack
+ * @line: The line on which the error occurred
+ * Return: Nothing
+ */
+void pall(stack_t **stack, unsigned int line)
+{
+	stack_t *current = NULL;
+	(void) line;
+
+	if (*stack)
+	{
+		current = *stack;
+		while (current != NULL)
+		{
+			printf("%d\n", current->n);
+			current = current->next;
+		}
+	}
+}
---
+++ push.c
@@ -0,0 +1,68 @@
+#include "monty.h"
+
+/**
+  * push - Adds a new node at the beginning of the stack
+  * @stack: head of the stack
+  * @param: param value
+  * Return: VOID
+  */
+void push(stack_t **stack, unsigned int param)
+{
+	stack_t *inode = NULL;
+
+	inode = malloc(sizeof(stack_t));
+	if (inode == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+
+	inode->n = param;
+	if (*stack)
+	{
+		inode->next = *stack;
+		inode->prev = (*stack)->prev;
+		(*stack)->prev = inode;
+		*stack = inode;
+		return;
+	}
+
+	inode->next = *stack;
+	inode->prev = NULL;
+	*stack = inode;
+}
+
+/**
+  * push_queue - Adds a new node at the end of the stack
+  * @stack: head of the stack
+  * @param: value to adds on the stack
+  * Return: VOID
+  */
+void push_queue(stack_t **stack, unsigned int param)
+{
+	stack_t *current = NULL, *inode = NULL;
+
+	inode = malloc(sizeof(stack_t));
+	if (inode == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+
+	inode->n = param;
+	if (*stack)
+	{
+		current = *stack;
+		while (current->next != NULL)
+			current = current->next;
+
+		inode->next = NULL;
+		inode->prev = current;
+		current->next = inode;
+		return;
+	}
+
+	inode->next = *stack;
+	inode->prev = NULL;
+	*stack = inode;
+}
